ecfef139c0427f0e53aab2b204cf8600cb2572b5
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2018-03-27 18:31:00 -0300

    parse ip range (wip)

--- main.c
+++ main.c
@@ -1,18 +1,63 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(int argc, char const *argv[]) {
+//Returns 0 if no range is passed. Returns 1 if range is passed, with start and end on params
+int parse_ip_range(char* start, char* end){
+	char* token;
+	char *bytes_start[4], *bytes_end[4];
+	int i = 0;
+	
+
+	//Separates IP's bytes by points
+	token = strtok(start, ".");
+	while(token){
+		bytes_start[i++] = token;
+		token = strtok(NULL, ".");
+	}
+
+	//Check for dashes on bytes
+	for(i = 0; i < 4; i++){
+		token = strtok(bytes_start[i], "-");
+		printf("%s\n", token );
+		bytes_start[i] = token;
+		token = strtok(NULL, "-");
+		if(token){
+			bytes_end[i] = token;
+		}
+		else{
+			bytes_end[i] = bytes_start[i];
+		}
+	}
+
+	for(i = 0; i < 4; i++){
+		printf("start:%s     end:%s\n", bytes_start[i], bytes_end[i]);
+	}
+
+	return 1;
+
+}
+
+int main(int argc, char *argv[]) {
   //in_addr struct has a single member s_addr
   struct in_addr addr;
-  char ip_input[16];
+  char* ip_input; char* port_input; char* ip_end;
+
+  if(argc < 3){
+  	printf("Error parsing params. Correct usage: porscan <IP address (or range)> <Port (range)>");
+  	exit(1);
+  }
+  ip_input = argv[1];
+  port_input = argv[2];
 
 
-  fgets(ip_input, sizeof(ip_input), stdin);
-  inet_aton(ip_input, &(addr));
 
-  printf("Hello %s\n", inet_ntoa(addr));
+  printf("%s\n", ip_input);
+
+  parse_ip_range(ip_input, ip_end);
+
   return 0;
 }
