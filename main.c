f93ad2fcf8bafb1a7037eb1a16575930a2106e17
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2018-03-26 22:23:18 -0300

    read string and  parse it into ip addr

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int int main(int argc, char const *argv[]) {
-  
+int main(int argc, char const *argv[]) {
+  //in_addr struct has a single member s_addr
+  struct in_addr addr;
+  char ip_input[16];
+
+
+  fgets(ip_input, sizeof(ip_input), stdin);
+  inet_aton(ip_input, &(addr));
+
+  printf("Hello %s\n", inet_ntoa(addr));
   return 0;
 }
