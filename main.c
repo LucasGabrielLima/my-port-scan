e651823f52796126de06660aae369992130ee39a
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2018-04-21 15:56:55 -0300

    scan hosts ports

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #define OFFSET 4
 
@@ -50,18 +51,23 @@ int *parse_ip_range(char* start){
 
 	// sprintf(range, "%s.%s.%s.%s %s.%s.%s.%s", bytes_start[0], bytes_start[1], bytes_start[2], bytes_start[3], bytes_end[0], bytes_end[1], bytes_end[2], bytes_end[3]);
 	return range;
+}
 
+void init_target(struct sockaddr_in *target, int port, char *ip){
+	target->sin_family = AF_INET;
+	target->sin_port = htons(port);
+	target->sin_addr.s_addr = inet_addr(ip);
 }
 
 int main(int argc, char *argv[]) {
-  //in_addr struct has a single member s_addr
-  struct in_addr addr;
-  char *ip_input; char *port_input; char *ip_end;
-	int *range; int *port_range;
-	int i, j, k, l, m;
+	struct sockaddr_in target;
+  char *ip_input; char *port_input; char host[16];
+	int *range;
+	int port_range[] = {1, 65535};
+	int i, j, k, l, p, sock, conn;
 
-  if(argc < 3){
-  	printf("Error parsing params. Correct usage: porscan <IP address (or range)> <Port (range)>");
+  if(argc < 2){
+  	printf("Error parsing params. Correct usage: porscan <IP address (or range)> [<Port range (xxx-yyy)>]");
   	exit(1);
   }
 
@@ -73,13 +79,40 @@ int main(int argc, char *argv[]) {
 
   range = parse_ip_range(ip_input);
 
+	for(i = 0; i < 8; i++){
+		printf("%d \n", range[i]);
+	}
+
+
 	//Iterates over IP and port ranges
 	for(i = range[0]; i <= range[0 + OFFSET]; i++){
 		for(j = range[1]; j <= range[1 + OFFSET]; j++){
 			for(k = range[2]; k <= range[2 + OFFSET]; k++){
 				for(l = range[3]; l <= range[3 + OFFSET]; l++){
-					for(m = port_range[0]; m <= port_range[1]; m++){
+					sprintf(host, "%d.%d.%d.%d", i, j, k, l);
+					printf("Scanning host %s\n", host );
+					
+					for(p = port_range[0]; p <= port_range[1]; p++){
+						sock = socket(AF_INET, SOCK_STREAM, 0);
+						if(sock < 0){
+							printf("Erro na criação do socket\n");
+							exit(1);
+						}
 
+
+						init_target(&target, p, host);
+
+						conn = connect(sock, (struct sockaddr *)&target, sizeof(target));
+						if(conn == 0){
+							printf("Port open: %d\n", p);
+							close(conn);
+						}
+						else{
+							printf("Port closed: %d\n", p);
+							close(conn);
+						}
+
+						close(sock);
 					}
 				}
 			}
